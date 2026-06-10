b9de77ba4cc92df549d5b458f2586fba23e5ccb4
Priyanka Pandit <priyankapandit0012gmail.com>
2026-06-10 16:42:21 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -1,164 +1,463 @@
+let allUserNotes = [];
 let db;
 
-const request = indexedDB.open("MinoNotesDB", 1);
+const request = indexedDB.open("MinoNotesDB", 2);
 
 request.onupgradeneeded = function (e) {
     db = e.target.result;
 
     if (!db.objectStoreNames.contains("users")) {
-        db.createObjectStore("users", { keyPath: "id" });
+        db.createObjectStore("users", { keyPath: "id", autoIncrement: true });
+    }
+
+    if (!db.objectStoreNames.contains("notes")) {
+        db.createObjectStore("notes", { keyPath: "noteId", autoIncrement: true });
     }
 };
 
 request.onsuccess = function (e) {
     db = e.target.result;
 
-    checkUser();
+    updateUI();
+    // loadNotes();
+    attachEvents();
 };
 
+// const addNoteBtn = document.getElementById("addNoteBtn");
+// const noteModal = document.getElementById("noteModal");
+
+addNoteBtn.addEventListener("click", () => {
+    noteModal.classList.remove("hidden");
+});
+
+
+const modal = document.getElementById("authModal");
+const modalTitle = document.getElementById("modalTitle");
+const submitBtn = document.getElementById("submitBtn");
+
+let mode = "login";
+
+document.getElementById("loginBtn").addEventListener("click", () => {
+    mode = "login";
+    modalTitle.textContent = "Login";
+    submitBtn.textContent = "Login";
+    modal.classList.remove("hidden");
+});
+
+document.getElementById("signupBtn").addEventListener("click", () => {
+    mode = "signup";
+    modalTitle.textContent = "Sign Up";
+    submitBtn.textContent = "Sign Up";
+    modal.classList.remove("hidden");
+});
+
+document.getElementById("closeBtn").addEventListener("click", () => {
+    modal.classList.add("hidden");
+});
+
+submitBtn.addEventListener("click", () => {
+
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (!username || !password) {
+        alert("Please fill all fields");
+        return;
+    }
+
+    if (mode === "signup") {
+        signup(username, password);
+    } else {
+        login(username, password);
+    }
+
+    modal.classList.add("hidden");
+});
+
+
+
+function updateUI() {
+
+
+    const username = localStorage.getItem("loggedUser");
+
+    const loginBtn = document.getElementById("loginBtn");
+    const signupBtn = document.getElementById("signupBtn");
+    const addNoteBtn = document.getElementById("addNoteBtn");
+    const userBox = document.getElementById("userBox");
+    const notesContainer = document.getElementById("notesContainer");
+    const welcomeTxt = document.getElementById("welcomeTxt");
+
+    if (username) {
+        loginBtn.style.display = "none";
+        signupBtn.style.display = "none";
+        welcomeTxt.style.display = "none";
+        addNoteBtn.classList.remove("hidden");
+        notesContainer.classList.remove("hidden");
+        userBox.innerHTML = `
+            <div class="text-lg font-semibold text-gray-800">
+                 Hi, ${username}
+            </div>
+        `;
+        loadNotes();
+    } else {
+        loginBtn.style.display = "block";
+        signupBtn.style.display = "block";
+        welcomeTxt.style.display = "block";
+        notesContainer.classList.add("hidden");
+        addNoteBtn.classList.add("hidden");
+        userBox.innerHTML = "";
+        notesContainer.innerHTML = ""; // clear data
+    }
+}
 
 function signup(username, password) {
-
     let tx = db.transaction("users", "readwrite");
     let store = tx.objectStore("users");
-
-    store.put({
-        id: 1,
-        username: username,
-        password: password
+    store.add({
+        id: Date.now(),
+        username,
+        password
     });
-
     tx.oncomplete = () => {
-        alert("Signup successful! Now login karo");
+        alert("Signup successful!");
+
+        // tx.oncomplete = () => {
+        //     alert("Signup successful!");
+        //     //sessionStorage.setItem("loggedUser", username); // temporary session
+        //     updateUI();
+        // };
+
+
     };
 }
 
-
 function login(username, password) {
-
     let tx = db.transaction("users", "readonly");
     let store = tx.objectStore("users");
+    let req = store.getAll();
+    req.onsuccess = function (e) {
+        //const user = req.result;
+        const users = e.target.result;
+        const user = users.find(u =>
+            u.username === username && u.password === password
+        );
 
-    let req = store.get(1);
+        if (user) {
 
-    req.onsuccess = function () {
-        const user = req.result;
-
-        if (user && user.username === username && user.password === password) {
-
-            localStorage.setItem("isLoggedIn", "true");
-            localStorage.setItem("username", username);
+            // 🔥 persist login
+            localStorage.setItem("loggedUser", username);
 
             alert("Login successful");
 
             updateUI();
+            // loadNotes();
 
         } else {
             alert("Invalid credentials");
         }
     };
 }
+window.addEventListener("load", () => {
 
+    const savedUser = localStorage.getItem("loggedUser");
 
-
+    if (savedUser) {
+        updateUI();
+        loadNotes();
+    }
+});
 function logout() {
-    localStorage.removeItem("isLoggedIn");
-    localStorage.removeItem("username");
-
+    localStorage.removeItem("loggedUser");
+    //  localStorage.removeItem("username"); 
+    // sessionStorage.removeItem("loggedUser");
+    document.getElementById("notesContainer").innerHTML = "";
     updateUI();
 }
 
 
-function checkUser() {
-    updateUI();
+// for notes
+
+
+
+
+function loadNotes() {
+
+    const container = document.getElementById("notesContainer");
+    container.innerHTML = "";
+
+    const username = localStorage.getItem("loggedUser");
+
+    if (!username) return;
+
+    let tx = db.transaction("notes", "readonly");
+    let store = tx.objectStore("notes");
+
+    let req = store.getAll();
+
+    req.onsuccess = function (e) {
+        const allNotes = e.target.result;
+        const username = localStorage.getItem("loggedUser");
+        allUserNotes = allNotes.filter(note => note.username === username);
+
+        //     userNotes.forEach((note, index) => {
+        //         const colors = [
+        //         "bg-white",
+        //         "bg-blue-50",
+        //         "bg-pink-50",
+        //         "bg-green-50",
+        //         "bg-yellow-50"
+        //         ];
+        //         const bg = colors[index % colors.length];
+
+        //     container.innerHTML += `
+        //     <div class="w-60 ${bg} shadow-md rounded-2xl p-4 border border-gray-200 hover:shadow-xl transition-all duration-300">
+
+        //     <h2 class="font-bold text-lg text-gray-800 mb-2">
+        //         ${note.title}
+        //     </h2>
+
+        //     <!-- horizontal line -->
+        //     <hr class="mb-2 border-gray-300">
+
+        //     <p class="text-gray-600 text-sm mb-3">
+        //         ${note.desc}
+        //     </p>
+
+        //     <div class="text-xs text-gray-400 mb-3">
+        //         ${note.date}
+        //     </div>
+
+        //     <button onclick="deleteNote(${note.noteId})"
+        //         class="bg-red-500 hover:bg-red-600 text-white px-3 py-1 text-sm rounded-lg transition">
+        //         Delete
+        //     </button>
+
+        // </div>
+        //         `;
+        //     });
+        renderNotes(allUserNotes);
+    };
+
 }
 
+function renderNotes(notes) {
+    const container = document.getElementById("notesContainer");
+    container.innerHTML = "";
 
-function updateUI() {
+    const colors = [
+        "bg-white",
+        "bg-blue-50",
+        "bg-pink-50",
+        "bg-green-50",
+        "bg-yellow-50"
+    ];
 
-    const isLoggedIn = localStorage.getItem("isLoggedIn");
-    const username = localStorage.getItem("username");
+    notes.forEach((note, index) => {
+        const bg = colors[index % colors.length];
 
-    const userBox = document.getElementById("userBox");
+        container.innerHTML += `
+        
+            <div onclick="openNote(${note.noteId})"
+                class="w-60 ${bg} shadow-md rounded-2xl p-4 border border-gray-200 hover:shadow-xl transition-all duration-300 cursor-pointer">
+            <h2 class="font-bold text-lg text-gray-800 mb-2">
+                ${note.title}
+            </h2>
 
-    if (isLoggedIn) {
+            <hr class="mb-2 border-gray-300">
 
-        userBox.innerHTML = `
-            <div class="relative group flex items-center gap-2 cursor-pointer">
+            <p class="text-gray-600 text-sm mb-3">
+                ${note.desc}
+            </p>
 
-                <img class="w-8 h-8 rounded-full"
-                     src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png">
-
-                <span class="text-gray-700 font-medium">
-                    ${username}
-                </span>
-
-                <span>▼</span>
-
-                <div class="absolute top-full right-0 mt-2 w-40 bg-white shadow-lg rounded-xl
-                            opacity-0 invisible group-hover:visible group-hover:opacity-100 transition">
-
-                    <button onclick="logout()"
-                        class="w-full text-left px-4 py-2 hover:bg-gray-100 text-red-500">
-                        Logout
-                    </button>
-
-                </div>
-
+            <div class="text-xs text-gray-400 mb-3">
+                ${note.date}
             </div>
+
+            <button onclick="event.stopPropagation(); deleteNote(${note.noteId})"
+                class="bg-red-500 hover:bg-red-600 text-white px-3 py-1 text-sm rounded-lg">
+                Delete
+            </button>
+
+        </div>
         `;
+    });
+}
 
-    } else {
+const colors = [
+    "bg-blue-50",
+    "bg-pink-50",
+    "bg-green-50",
+    "bg-yellow-50",
+    "bg-purple-50"
+];
 
-        userBox.innerHTML = `
-            <div class="flex gap-3">
-                <button onclick="openLogin()" class="bg-blue-500 text-white px-4 py-1 rounded">Login</button>
-                <button onclick="openSignup()" class="bg-gray-700 text-white px-4 py-1 rounded">Sign Up</button>
-            </div>
-        `;
+function openNote(noteId) {
+
+    const popup = document.getElementById("popupCard");
+
+    colors.forEach(color => popup.classList.remove(color));
+
+    const randomColor =
+        colors[Math.floor(Math.random() * colors.length)];
+
+    popup.classList.add(randomColor);
+
+    const note = allUserNotes.find(
+        note => note.noteId === noteId
+    );
+
+    if (!note) return;
+
+    document.getElementById("viewTitle").textContent =
+        note.title;
+
+    document.getElementById("viewDesc").textContent =
+        note.desc;
+
+    document.getElementById("viewDate").textContent =
+        note.date;
+
+    document
+        .getElementById("viewNoteModal")
+        .classList.remove("hidden");
+}
+
+function closeViewNote() {
+    document
+        .getElementById("viewNoteModal")
+        .classList.add("hidden");
+}
+
+// Search logic start here
+
+
+function handleSearch(e) {
+    let value = typeof e === "string" ? e : e.target.value;
+
+    console.log("Searching:", value);
+    console.log("All Notes:", allUserNotes);
+
+    value = value.toLowerCase().trim();
+
+    const filtered = allUserNotes.filter(note =>
+        note.title.toLowerCase().includes(value)
+    );
+
+    console.log("Filtered:", filtered);
+
+    renderNotes(filtered);
+}
+
+const searchInput = document.getElementById("searchInput");
+
+searchInput.addEventListener("input", handleSearch);
+
+
+searchInput.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        handleSearch(e.target.value);
     }
+});
+window.addEventListener("load", () => {
+    const searchInput = document.getElementById("searchInput");
+
+    searchInput.addEventListener("input", handleSearch);
+
+    searchInput.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+            handleSearch(e);
+        }
+    });
+});
+
+
+
+
+// document.getElementById("searchInput").addEventListener("input", function (e) {
+//     const value = e.target.value.toLowerCase().trim();
+
+//     if (!value) {
+//         renderNotes(allUserNotes);
+//         return;
+//     }
+
+//     const filtered = allUserNotes.filter(note =>
+//         note.title.toLowerCase().includes(value)
+//     );
+
+//     renderNotes(filtered);
+// });
+
+
+// search logic end here
+
+
+
+function attachEvents() {
+
+    const addNoteBtn = document.getElementById("addNoteBtn");
+    const noteModal = document.getElementById("noteModal");
+    const saveNoteBtn = document.getElementById("saveNoteBtn");
+
+    addNoteBtn.onclick = () => {
+        noteModal.classList.remove("hidden");
+    };
+
+    saveNoteBtn.addEventListener("click", () => {
+
+        const title = document.getElementById("noteTitle").value;
+        const desc = document.getElementById("noteDesc").value;
+        const username = localStorage.getItem("loggedUser");
+
+        if (!title || !desc) {
+            alert("Please fill all fields");
+            return;
+        }
+
+
+        if (!username) {
+            alert("Please login first");
+            return;
+        }
+
+        let tx = db.transaction("notes", "readwrite");
+        let store = tx.objectStore("notes");
+
+        const note = {
+            username: username,
+            title,
+            desc,
+            date: new Date().toLocaleString()
+        };
+
+        store.add(note);
+
+        tx.oncomplete = () => {
+
+            // ✔ clear inputs
+            document.getElementById("noteTitle").value = "";
+            document.getElementById("noteDesc").value = "";
+
+            // ✔ hide popup
+            noteModal.classList.add("hidden");
+
+            // ✔ refresh notes
+            loadNotes();
+        };
+    });
 }
 
+function deleteNote(id) {
 
-function openLogin() {
-    const username = prompt("Enter username");
-    const password = prompt("Enter password");
+    let tx = db.transaction("notes", "readwrite");
+    let store = tx.objectStore("notes");
 
-    login(username, password);
+    store.delete(id);
+
+    tx.oncomplete = () => {
+        loadNotes();
+    };
 }
 
-function openSignup() {
-    const username = prompt("Choose username");
-    const password = prompt("Choose password");
-
-    signup(username, password);
-}
-
-
-
-  const userBtn = document.getElementById("userBtn");
-  const dropdown = document.getElementById("dropdown");
-
-  let open = false;
-
-  userBtn.addEventListener("click", (e) => {
-    e.stopPropagation();
-    open = !open;
-
-    if (open) {
-      dropdown.classList.remove("hidden");
-    } else {
-      dropdown.classList.add("hidden");
-    }
-  });
-
-  // click outside → close dropdown
-  document.addEventListener("click", () => {
-    dropdown.classList.add("hidden");
-    open = false;
-  });
-
-  // prevent dropdown from closing when clicking inside
-  dropdown.addEventListener("click", (e) => {
-    e.stopPropagation();
-  });
